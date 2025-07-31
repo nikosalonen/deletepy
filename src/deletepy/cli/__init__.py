f412eaa442b35c7451640a3e3e0346ab990fd7f8
Niko Salonen <nikotsalonen@gmail.com>
2025-07-31 10:02:52 +0300

    refactor: clean up import statements in cli __init__.py

--- src/deletepy/cli/__init__.py
+++ src/deletepy/cli/__init__.py
@@ -1,8 +1,7 @@
 """CLI module for Auth0 user management."""
 
-from .csv_commands import handle_csv_command
+from .csv_commands import handle_csv_command, parse_csv_args, print_csv_usage
 from .csv_commands import main as csv_main
-from .csv_commands import parse_csv_args, print_csv_usage
 from .validators import (
     validate_args,
     validate_connection_type,
